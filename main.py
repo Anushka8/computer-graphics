cd48fea9f85a397e0070bcf355614f6b61b94a6f
Anushka8 <anushkachuri8@gmail.com>
2023-11-07 18:57:10 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,17 +6,48 @@ import numpy as np
 
 
 def default_action():
-    # place your code here
+    # # place your code here
     myEngine.win.clearFB(0, 0, 0)
     myEngine.clearModelTransform()
     myEngine.translate(400, 190, 0)
-    print(myEngine.model_matrix)
     myEngine.pushTransform()
-    print(myEngine.transformation_stack[-1])
     myEngine.translate(0, 0.5, 0)
     myEngine.scale(120, 30, 1)
     myEngine.pushTransform()
-    myEngine.drawTriangleC(cube, cube_idx, 255, 0, 0)
+    myEngine.drawTriangleC(cube, cube_idx, 255, 90, 0)
+    myEngine.popTransform()
+
+    # myEngine.translate(100,0, 0)
+    myEngine.clearModelTransform()
+    myEngine.translate(0, 130, 0)
+    myEngine.pushTransform()
+    # myEngine.translate(50, 50, 0)
+    myEngine.scale(80, 200, 1)
+    myEngine.pushTransform()
+    myEngine.drawTriangleC(cylinder, cylinder_idx, 255, 30, 0)
+    myEngine.popTransform()
+
+    myEngine.clearModelTransform()
+    myEngine.translate(0, 100, 0)
+    myEngine.pushTransform()
+    myEngine.translate(0, 0.5, 0)
+    myEngine.scale(120, 30, 1)
+    myEngine.pushTransform()
+    myEngine.drawTriangleC(cube, cube_idx, 255, 20, 0)
+    myEngine.popTransform()
+
+    myEngine.clearModelTransform()
+    myEngine.translate(0, 80, 0)
+    myEngine.pushTransform()
+    myEngine.rotate_x(20)
+    myEngine.rotate_y(85)
+    myEngine.rotate_z(75)
+    myEngine.translate(0, 0.5, 0)
+    myEngine.scale(80, 60, 1)
+    # myEngine.rotate_y(80)
+    # myEngine.rotate_x(120)
+    myEngine.pushTransform()
+    myEngine.drawTriangleC(cube, cube_idx, 255, 20, 0)
     myEngine.popTransform()
 
 
