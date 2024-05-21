241e03e6cde0a665e24a7a91fec4eb13a346369e
Anushka Churi <anushkachuri8@gmail.com>
2024-05-21 15:03:27 -0400

    3D pencil

--- main.py
+++ main.py
@@ -2,65 +2,55 @@ from rit_window import *
 from cgI_engine import *
 from vertex import *
 from clipper import *
-from shapes import *
+from shapes_new import *
 import numpy as np
-
-
-def drawPedistal(r, g, b):
-    # pole
-    myEngine.pushTransform()
-    myEngine.scale(0.6, 2.0, 0.6)
-    myEngine.drawTrianglesC(cylinder, cylinder_idx, 255, 255, 0, 255, 0, 0)
-    myEngine.popTransform()
-
-    # top
-    myEngine.pushTransform()
-    myEngine.translate(0.0, 1.0, 0.0)
-    myEngine.pushTransform()
-    myEngine.scale(0.9, 0.1, 0.9)
-    myEngine.drawTrianglesC(cube, cube_idx, 255, 255, 0, 255, 0, 0)
-    myEngine.popTransform()
-
-    # showcaae cube
-    myEngine.pushTransform()
-    myEngine.translate(0.0, 0.45, 0.0)
-    myEngine.pushTransform()
-    myEngine.scale(0.4, 0.4, 0.4)
-    myEngine.rotatex(45.0)
-    myEngine.rotatey(45.0)
-    myEngine.drawTrianglesC(cube, cube_idx, 0, 255, 0, 0, 0, 0)
-    myEngine.popTransform()
-    myEngine.popTransform()
-    myEngine.popTransform()
-
-    # bottom
-    myEngine.pushTransform()
-    myEngine.translate(0.0, -1.0, 0.0)
-    myEngine.pushTransform()
-    myEngine.scale(0.9, 0.1, 0.9)
-    myEngine.drawTrianglesC(cube, cube_idx, 255, 255, 0, 255, 0, 0)
-    myEngine.popTransform()
-    myEngine.popTransform()
+from PIL import Image
 
 
 def default_action():
+    # create your scene here
     # clear the FB
-    myEngine.win.clearFB(0, 0, 0)
-    myEngine.defineClipWindow(1.0, -1.0, 1.0, -1.0)
-    # myEngine.defineViewWindow(700, 0, 700, 0)
+    myEngine.win.clearFB(.15, .15, .45)
 
     # set up your camera
-    myEngine.setCamera(glm.vec3([0.0, 2.0, 4.0]), glm.vec3([0, 0, 0]), glm.vec3([0, 1, 0]))
-    myEngine.setOrtho(-6.0, 6.0, -6.0, 6.0, 1.0, 10.0)
+    myEngine.setCamera(glm.vec3([0.0,0.0,2.0]), glm.vec3([0,0,20]), glm.vec3([0,1,0]))
+    myEngine.setOrtho(-3.0, 3.0, -3.0, 3.0, -3.0, 3.0)
 
-    # position
-    myEngine.pushTransform()
-    myEngine.translate(2, 0, -3)
-    drawPedistal(0, 255, 0)
+    # wooden cone
+    myEngine.clearModelTransform()
+    im = Image.open("wood.jpg")
+    myEngine.translate(0.46, 1.55, 1.5)
+    myEngine.rotatey(30)
+    myEngine.rotatex(30)
+    myEngine.scale(0.5, 0.68, 0.5)
+    myEngine.drawTrianglesTextures(cone_new, cone_new_idx, cone_new_uv, im)
 
-    myEngine.pushTransform()
-    myEngine.translate(-2, 0, -5.0)
-    drawPedistal(0, 0, 255)
+    # main pencil body
+    myEngine.clearModelTransform()
+    im = Image.open("pencil.jpg")
+    myEngine.translate(0.0, 0.0, 0.0)
+    myEngine.rotatey(30)
+    myEngine.rotatex(30)
+    myEngine.scale(0.5, 2.8, 0.5)
+    myEngine.drawTrianglesTextures(cylinder_new, cylinder_new_idx, cylinder_new_uv, im)
+
+    # metal part
+    myEngine.clearModelTransform()
+    im = Image.open("metal.jpg")
+    myEngine.translate(-0.36, -1.27, -0.62)
+    myEngine.rotatey(30)
+    myEngine.rotatex(30)
+    myEngine.scale(0.5, 0.1, 0.5)
+    myEngine.drawTrianglesTextures(cylinder_new, cylinder_new_idx, cylinder_new_uv, im)
+
+    # eraser
+    myEngine.clearModelTransform()
+    im = Image.open("eraser.jpg")
+    myEngine.translate(-0.4, -1.35, -0.62)
+    myEngine.rotatey(30)
+    myEngine.rotatex(30)
+    myEngine.scale(0.5, 0.3, 0.5)
+    myEngine.drawTrianglesTextures(cylinder_new, cylinder_new_idx, cylinder_new_uv, im)
 
 
 window = RitWindow(800, 800)
